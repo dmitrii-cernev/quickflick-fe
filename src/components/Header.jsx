25a3e4cd493a9ab0c15fa8344d8d20b60edbea22
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2023-11-22 21:51:15 +0100

    attempt to resolve issue with logo on Netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from "../media/logo.png"
+
 export function Header() {
     return (
         <header className="bg-white shadow-lg p-6">
             <div className="container mx-auto flex items-center">
-                <img src="/src/media/logo.png" alt="QuickFlick" className="h-8 w-8 mr-4"/>
+                <img src={logo} alt="QuickFlick" className="h-8 w-8 mr-4"/>
                 <h1 className="text-2xl font-semibold text-gray-800">QuickFlick</h1>
             </div>
         </header>
