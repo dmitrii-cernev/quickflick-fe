7d08695a91787ba6603b635e445829363e9fdedd
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2023-11-23 22:12:53 +0100

    fix host

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,8 +29,7 @@ export default function Hero() {
         try {
             setIsLoading(true)
             const encodedLink = encodeURIComponent(link)
-            let host = `3.231.164.83:8080`;
-            await fetch(`${host}/process?url=${encodedLink}`)
+            await fetch(`http://3.231.164.83:8080/process?url=${encodedLink}`)
                 .then(response => response.json())
                 .then(response => {
                     setApiResponse(response)
