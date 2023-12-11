b06149c8014ea0ffda527289b9987b9713992588
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2023-12-11 22:27:21 +0100

    fix left margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import logo from "../media/logo.svg"
 export function Header() {
     return (
         <header className="bg-white shadow-lg p-5">
-            <div className="container mx-auto flex items-center ml-8">
+            <div className="container mx-auto flex items-center">
                 <img src={logo} alt="MiniMemo" className="h-10 w-10 mr-4"/>
-                <h1 className="text-2xl font-semibold text-gray-800">MiniMemo</h1>
+                <h1 className="text-[1.75rem] font-semibold text-gray-800">MiniMemo</h1>
             </div>
         </header>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -74,29 +74,31 @@ export default function Hero() {
     }, [ip, transcriptionsRetrieved, transcriptions]);
 
 
-    return (<div className={"hero p-8 min-h-screen flex items-center justify-center"}>
-        <div className={"max-w-screen-lg"}>
-            <h1 className={"animate-fade-down animate-duration-700 flex flex-wrap text-5xl font-semibold text-white gap-x-4"}>Summarize
-                short videos from <b
-                className="min-w-[250px]">{supportedServices.word}</b>
-            </h1>
+    return (
+        <div className={"p-8 min-h-screen flex items-center justify-center"}>
+            <div className={"max-w-screen-lg"}>
+                <h1 className={"animate-fade-down animate-duration-700 flex flex-wrap text-5xl font-semibold text-white gap-x-4"}>Summarize
+                    short videos from <b
+                        className="min-w-[250px]">{supportedServices.word}</b>
+                </h1>
 
-            <InputForm value={link} onChange={handleInputChange} disabled={isValidLink && !isLoading}
-                       onClick={sendRequest}/>
+                <InputForm value={link} onChange={handleInputChange} disabled={isValidLink && !isLoading}
+                           onClick={sendRequest}/>
 
-            {isLoading && <LoadingSpinner/>}
+                {isLoading && <LoadingSpinner/>}
 
-            {link && !isValidLink && (
-                <p className="bg-white text-red-500 w-80 mt-2 p-1 rounded-lg">Please enter a valid TikTok or Instagram
-                    link.</p>)}
+                {link && !isValidLink && (
+                    <p className="bg-white text-red-500 w-80 mt-2 p-1 rounded-lg">Please enter a valid TikTok or
+                        Instagram
+                        link.</p>)}
 
-            {apiResponse.title && (
-                <ResponseShow apiResponse={apiResponse} onClick={toggleTranscription}
-                              showTranscription={showTranscription}/>
-            )}
-            {transcriptionsRetrieved && transcriptions.length > 0 && (
-                <TranscriptionsTable transcriptions={transcriptions}/>
-            )}
-        </div>
-    </div>)
+                {apiResponse.title && (
+                    <ResponseShow apiResponse={apiResponse} onClick={toggleTranscription}
+                                  showTranscription={showTranscription}/>
+                )}
+                {transcriptionsRetrieved && transcriptions.length > 0 && (
+                    <TranscriptionsTable transcriptions={transcriptions}/>
+                )}
+            </div>
+        </div>)
 }
