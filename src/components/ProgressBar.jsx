b35be639d85904c26e6bf19b1181d565f8c2704a
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2023-12-21 19:14:47 +0100

    update progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,13 +1,13 @@
 export default function ProgressBar(props) {
     return (
-        <div className='relative m-4 h-3.5 w-9/12 bg-gray-200 rounded-full animate-fade'>
+        <div className='relative m-4 h-4 w-9/12 bg-gray-100 shadow-md rounded-full animate-fade'>
             <div
                 style={{width: `${props.value}%`}}
-                className={'h-full rounded-full bg-purple-700 animate-pulse animate-duration-[2000ms]'}>
+                className={'h-full rounded-full bg-purple-500 animate-pulse animate-duration-[2500ms]'}>
             </div>
-            <div className="absolute inset-0 flex items-center justify-center text-white text-sm">
-                {props.value}%
+            <div
+                className="absolute inset-0 flex items-center justify-center">
+                <span className={"mix-blend-difference text-neutral-400 font-light"}>{props.value}%</span>
             </div>
-        </div>
-    );
+        </div>);
 }
