76cd47c8a51429d44c43d5a95be1bb4a2b54bdee
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2023-12-27 16:41:38 +0100

    toggle navbar on mobile devices on clicks

--- src/components/AppNavLink.jsx
+++ src/components/AppNavLink.jsx
@@ -1,10 +1,18 @@
 import {useLinkClickHandler} from "react-router-dom";
 import {NavbarLink} from "flowbite-react";
+import {useNavbarContext} from "flowbite-react/lib/esm/components/Navbar/NavbarContext.js";
 
 export default function AppNavLink(props) {
+    const {isOpen, setIsOpen} = useNavbarContext();
+    const toggleNavbar = () => setIsOpen(!isOpen);
     const clickHandler = useLinkClickHandler(props.to);
 
-    return <span onClick={clickHandler}>
+    function togg(event) {
+        toggleNavbar();
+        clickHandler(event);
+    }
+
+    return <span onClick={togg}>
         <NavbarLink href={props.to} className={props.className}>
             {props.text}
         </NavbarLink>
